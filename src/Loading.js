e7f97cc4f3482cdd1fe93f7246344bbef5099215
moran711 <59802190+moran711@users.noreply.github.com>
2020-05-03 16:59:14 +0300

    Add files via upload

---
+++ src/Loading.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <div className="lds-roller" style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
